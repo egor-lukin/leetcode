b0a6723017c422a2de5f45df3ceb44cfee25b713
Lukin Egor <lukin.net@gmail.com>
2022-07-18 00:58:28 +0400

    add medium#7

---
+++ src/medium/7_reverse_integer/source.rb
@@ -0,0 +1,28 @@
+require 'byebug'
+
+# https://leetcode.com/problems/reverse-integer/
+class MediumReverseInteger
+  MAX_NEGATIVE = 2147483648
+  MAX_POSITIVE = 2147483647
+
+  # @param {Integer} x
+  # @return {Integer}
+  def reverse x
+    rev_x = 0
+    origin_x = x
+    x = x.abs
+    while (x >= 1 && x > 0) || (x <= -1 && x < 0)
+
+      mod = x % 10
+      x = x / 10
+
+      rev_x = rev_x * 10 + mod
+      return 0 if (rev_x > MAX_POSITIVE && origin_x > 0) ||
+                  (rev_x > MAX_NEGATIVE && origin_x < 0)
+    end
+
+    return -rev_x if origin_x < 0
+
+    rev_x
+  end
+end
