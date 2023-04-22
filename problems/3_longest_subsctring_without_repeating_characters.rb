41d3504aabef9ba91c4760299c1d9f472c298beb
Egor Lukin <lukin.net@gmail.com>
2023-04-22 14:43:53 +0400

    solve #3

--- problems/3_longest_subsctring_without_repeating_characters.rb
+++ problems/3_longest_subsctring_without_repeating_characters.rb
@@ -6,26 +6,17 @@ class Problem
   # @param {String} s
   # @return {Integer}
   def self.length_of_longest_substring(s)
-    return 0 if s == ''
-
-    letters = {}
-    max = 1
-    only_uniq_letters = true
-    s.split('').each_with_index do |sym, idx|
-      # binding.pry
-      if letters[sym].nil?
-        letters[sym] = idx
-      else
-        only_uniq_letters = false
-        prev_idx = letters[sym]
-        distance = idx - prev_idx
-        max = distance if distance > max
-
-        letters[sym] = idx
+    set = Set.new
+    left = 0
+    max = 0
+    s.length.times do |right|
+      while set.add?(s[right]).nil?
+        set.delete(s[left])
+        left += 1
       end
-    end
 
-    return s.size if only_uniq_letters
+      max = [max, right - left + 1].max
+    end
 
     max
   end
@@ -33,13 +24,14 @@ end
 
 describe Problem do
   specify do
-    # expect(described_class.length_of_longest_substring('abcabcbb')).to eq(3)
-    # expect(described_class.length_of_longest_substring('bbbbb')).to eq(1)
-    # expect(described_class.length_of_longest_substring('pwwkew')).to eq(3)
-    # expect(described_class.length_of_longest_substring(' ')).to eq(1)
-    # expect(described_class.length_of_longest_substring('')).to eq(0)
-    # expect(described_class.length_of_longest_substring('au')).to eq(2)
+    expect(described_class.length_of_longest_substring('abcabcbb')).to eq(3)
+    expect(described_class.length_of_longest_substring('bbbbb')).to eq(1)
+    expect(described_class.length_of_longest_substring('pwwkew')).to eq(3)
+    expect(described_class.length_of_longest_substring(' ')).to eq(1)
+    expect(described_class.length_of_longest_substring('')).to eq(0)
+    expect(described_class.length_of_longest_substring('au')).to eq(2)
     expect(described_class.length_of_longest_substring('baab')).to eq(2)
-    # expect(described_class.length_of_longest_substring('aab')).to eq(2)
+    expect(described_class.length_of_longest_substring('aab')).to eq(2)
+    expect(described_class.length_of_longest_substring('dvdf')).to eq(3)
   end
 end
